6645a7c67de6d4b545574886230cfac48b5d3bb3
篠田弥樹 <miki@shinodawataruju-no-MacBook-Pro.local>
2015-10-04 12:02:39 +0900

    Initial Commit

---
+++ TechPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechPad
+//
+//  Created by 篠田弥樹 on 2015/10/04.
+//  Copyright (c) 2015年 篠田弥樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
